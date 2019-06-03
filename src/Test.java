7e4aa5e55c2d2f446e33a3ee4ec981831aecc0a2
xiaozhiyuan <xzyuan93@163.com>
2019-06-03 12:59:28 +0800

    first commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @Auther: yuan
+ * @Date: 2019/6/3 12:53
+ * @Description:
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String s = "hello git";
+        System.out.println(s);
+    }
+}
