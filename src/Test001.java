6a9ddfc35e631e4d51a4587f5c40c62f52bf015b
xiaozhiyuan <xzyuan93@163.com>
2019-06-03 13:21:58 +0800

    新建Test001

---
+++ src/Test001.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: yuan
+ * @Date: 2019/6/3 13:20
+ * @Description:
+ */
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("hello test001");
+    }
+}
